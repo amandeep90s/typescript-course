ec61a2c22f48ca8cc5c7133446dbf25741586314
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-15 19:23:43 +0530

    chore: working with numner, strings & booleans

--- section-02/app.ts
+++ section-02/app.ts
@@ -1,9 +1,20 @@
-function add(number1: number, number2: number) {
-  return number1 + number2;
+function add(
+  number1: number,
+  number2: number,
+  showResult: boolean,
+  phrase: string
+) {
+  const result = number1 + number2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 22;
 const number2 = 5;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
