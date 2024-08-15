9d205eddae4b3b5b34b199d1e3446eabbe52c7fb
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-15 20:12:28 +0530

    chore:Working with enums

--- section-02/app.ts
+++
@@ -1,9 +0,0 @@
-// Tuple is used for defined number of elements with fixed types
-
-const tupleExample: [number, string] = [1, 'Hello'];
-// Exception case in tuple scenario
-// tupleExample.push(2);
-// tupleExample.push('World');
-
-console.log(tupleExample);
-console.log(tupleExample.length);
