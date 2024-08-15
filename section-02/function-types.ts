7410c895912ab6b1bd026be000ba2dc282431057
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-15 21:44:28 +0530

    chore: Function types and callbacks

--- section-02/function-types.ts
+++ section-02/function-types.ts
@@ -2,8 +2,8 @@ function printSum(a: number, b: number) {
   return a + b;
 }
 
-function printText(text: string) {
-  return text;
+function printResult(text: number) {
+  console.log(text);
 }
 
 let output: (a: number, b: number) => number;
@@ -11,3 +11,9 @@ output = printSum;
 // Error: Type 'string' is not assignable to type 'number'
 // output = printText;
 console.log(output(10, 5));
+
+function addAndhandle(a: number, b: number, cb: (n: number) => void) {
+  const result = a + b;
+  cb(result);
+}
+addAndhandle(10, 20, printResult);
