53929199d0eb2acdcd2f24ae273f361b3a615a93
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-15 19:38:20 +0530

    chore: Object types

--- section-02/app.ts
+++ section-02/app.ts
@@ -1,20 +1,12 @@
-function add(
-  number1: number,
-  number2: number,
-  showResult: boolean,
-  phrase: string
-) {
-  const result = number1 + number2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+// const person: { name: string; age: number } = {
+//   name: 'Aman',
+//   age: 30,
+// };
 
-const number1 = 22;
-const number2 = 5;
-const printResult = true;
-const resultPhrase = 'Result is: ';
+// Better syntax
+const person = {
+  name: 'Aman',
+  age: 30,
+};
 
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
