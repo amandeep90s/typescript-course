e716debb0159881348bf298abb715a02603e19b7
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-16 21:43:10 +0530

    chore: Private and public access modifiers

--- section-05/src/app.ts
+++ section-05/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -8,7 +9,19 @@ class Department {
   describe() {
     console.log(`Department: ${this.name}`);
   }
+
+  addEmployees(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeesInformation() {
+    console.log("Total employees count is " + this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 accounting.describe();
+accounting.addEmployees("Max");
+accounting.addEmployees("Manu");
+accounting.printEmployeesInformation();
