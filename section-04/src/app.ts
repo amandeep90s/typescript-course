cce2e99991044261bf9ba8e686f017ef00ae5881
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-16 17:59:25 +0530

    chore: let and const

---
+++ section-04/src/app.ts
@@ -0,0 +1,9 @@
+const myName = "Amandeep Singh";
+// myName = "Aman"; // Error: Cannot assign to 'myName' because it is a read-only property.
+let age = 30;
+if (age > 50) {
+  const myFavSport = "Cricket"; // Block-scoped variable
+  age = 50;
+}
+console.log(myName, age); // Amandeep Singh 30
+// console.log(myFavSport); // Error: Cannot find name 'myFavSport'.
