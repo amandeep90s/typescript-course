d6eff01343f822737d96a44d223501650e515a5a
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-17 22:29:18 +0530

    chore: Optional chaining

--- section-06/src/app.ts
+++ section-06/src/app.ts
@@ -142,3 +142,12 @@ interface ErrorContainer {
 const errorBag: ErrorContainer = {
   email: "Not a valid email",
 };
+
+// Optional chaining
+const fetchedUserData = {
+  id: "u1",
+  name: "Aman",
+  job: { title: "CEO", description: "My own company" },
+};
+
+console.log(fetchedUserData?.job?.title);
