de05026d3684545ac8935250c481317b5949ae4c
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-17 19:27:33 +0530

    chore: Readonly interface properties

--- section-05/src/app.ts
+++ section-05/src/app.ts
@@ -1,6 +1,6 @@
 // Interface described the structure of an object.
 interface Greetable {
-  name: string;
+  readonly name: string; // readonly: can't be changed after object initialization (default)
   greet(phrase: string): void;
 }
 
@@ -20,6 +20,7 @@ class Person implements Greetable {
 
 // We can use interface as a type to define a variable or a parameter
 const user: Greetable = new Person("Aman", 25);
+// user.name = "Babbu"; // Error: Cannot assign to 'name' because it is a read-only property
 console.log(user);
 
 user.greet("Hello, I am ");
