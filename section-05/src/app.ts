257fbb006e81b1bb999c1bbc27d1e3935f526a46
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-17 19:24:22 +0530

    chore: Using interfaces with classes

--- section-05/src/app.ts
+++ section-05/src/app.ts
@@ -1,16 +1,25 @@
 // Interface described the structure of an object.
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
-const user: Person = {
-  name: "Aman",
-  age: 34,
+class Person implements Greetable {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
   greet(phrase: string) {
     console.log(phrase + this.name);
-  },
-};
+  }
+}
+
+// We can use interface as a type to define a variable or a parameter
+const user: Greetable = new Person("Aman", 25);
+console.log(user);
 
 user.greet("Hello, I am ");
