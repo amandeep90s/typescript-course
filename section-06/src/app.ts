d6267ac6bb8cf4c570960a2a9049017f09f9459d
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-17 20:28:00 +0530

    chore: More on type guards

--- section-06/src/app.ts
+++ section-06/src/app.ts
@@ -1,3 +1,5 @@
+// Type guards
+
 type Admin = {
   name: string;
   privileges: string[];
@@ -14,11 +16,28 @@ type Employee = {
 
 type ElevatedEmployee = Admin & Employee;
 
+type UnknownEmployee = Employee | Admin;
+
 const e1: ElevatedEmployee = {
   name: "Max",
   privileges: ["create-server"],
   startDate: new Date(),
 };
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name", emp.name);
+  // Type guard with object
+  if ("privileges" in emp) {
+    console.log("Privileges", emp.privileges);
+  }
+  // Type guard with object
+  if ("startDate" in emp) {
+    console.log("Start Date", emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({ name: "Aman", startDate: new Date() });
 
 type Combinable = string | number;
 type Numeric = number | boolean;
@@ -27,3 +46,43 @@ type Universal = Combinable & Numeric;
 
 // From both types it took common types
 const a: Universal = 11; // number
+
+function add(a: Combinable, b: Combinable) {
+  // Type guard with typeof
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo", amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // Type guard with instanceof
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
