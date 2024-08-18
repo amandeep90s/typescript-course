a0ce8f893a75ffd22d6236de468a5b75417d8f9a
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-18 22:34:18 +0530

    chore: Generic utility types

--- section-07/src/app.ts
+++ section-07/src/app.ts
@@ -34,3 +34,27 @@ console.log(numberStorage.getItems());
 // objectStorage.addItem({ name: 'Taran' });
 // objectStorage.removeItem(amanObj);
 // console.log(objectStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoall(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}; // Partial is a generic type that makes all properties of CourseGoal optional.
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Aman', 'Taran']; // Readonly is a generic type that makes the array readonly.
+// names.push('Manu'); // This will not work because names is a readonly array.
+
+const person: Readonly<{ name: string }> = { name: 'Aman' }; // Readonly is a generic type that makes the object readonly.
+// person.name = 'Taran'; // This will not work because person is a readonly object.
