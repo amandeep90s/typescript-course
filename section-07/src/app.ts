d6d8ffb4a43b25c8bd0a1461d77df67192b7ca67
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-18 22:08:44 +0530

    chore: Another generic function

--- section-07/src/app.ts
+++ section-07/src/app.ts
@@ -12,3 +12,18 @@ function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
 const mergeObj = merge({ name: 'Aman', hobbies: ['Sports'] }, { age: 34 });
 // mergeObj.name; // This will not work because merge function is returning object type and not the object with name property.
 console.log(mergeObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
+console.log(countAndDescribe(['Sports', 'Cooking']));
