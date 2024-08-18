207188baae4a9d125a0576cac4d6226eba82deb3
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-18 22:23:43 +0530

    chore: Generic classes

--- section-07/src/app.ts
+++ section-07/src/app.ts
@@ -1,40 +1,36 @@
-// Generics - Its a way to create reusable components that can work with any data type and not just one data type.
-// const names: Array<string> = []; // string[]
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
 
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("This is done"), 2000);
-// });
-
-function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-  return { ...objA, ...objB };
-}
-
-const mergeObj = merge({ name: 'Aman', hobbies: ['Sports'] }, { age: 34 });
-// mergeObj.name; // This will not work because merge function is returning object type and not the object with name property.
-console.log(mergeObj);
-
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = 'Got no value';
-  if (element.length === 1) {
-    descriptionText = 'Got 1 element';
-  } else if (element.length > 1) {
-    descriptionText = `Got ${element.length} elements`;
+  addItem(item: T) {
+    this.data.push(item);
   }
-  return [element, descriptionText];
-}
-console.log(countAndDescribe(['Sports', 'Cooking']));
 
-// keyof
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return 'Value: ' + obj[key];
-}
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
 
-const output = extractAndConvert({ name: 'Aman' }, 'name');
-console.log(output);
+  getItems() {
+    return [...this.data];
+  }
+}
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Aman');
+textStorage.addItem('Taran');
+textStorage.removeItem('Aman');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.removeItem(1);
+console.log(numberStorage.getItems());
+
+// const objectStorage = new DataStorage<object>();
+// const amanObj = { name: 'Aman' };
+// objectStorage.addItem(amanObj);
+// objectStorage.addItem({ name: 'Taran' });
+// objectStorage.removeItem(amanObj);
+// console.log(objectStorage.getItems());
---
+++ section-07/src/generics.ts
@@ -0,0 +1,40 @@
+// Generics - Its a way to create reusable components that can work with any data type and not just one data type.
+// const names: Array<string> = []; // string[]
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("This is done"), 2000);
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return { ...objA, ...objB };
+}
+
+const mergeObj = merge({ name: 'Aman', hobbies: ['Sports'] }, { age: 34 });
+// mergeObj.name; // This will not work because merge function is returning object type and not the object with name property.
+console.log(mergeObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+// keyof
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
+}
+
+const output = extractAndConvert({ name: 'Aman' }, 'name');
+console.log(output);
