1bf7174622274ed9404d88864e2825da1db4e6b0
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-18 20:50:03 +0530

    chore: Builtin generics

--- section-07/src/app.ts
+++ section-07/src/app.ts
@@ -1,1 +1,6 @@
-// Code goes here!
+// Generics - Its a way to create reusable components that can work with any data type and not just one data type.
+const names: Array<string> = []; // string[]
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("This is done"), 2000);
+});
