2aa7ca610290145f74ecdf4051abc7fe488a86c4
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-18 21:48:38 +0530

    chore: Working with constraints

--- section-07/src/app.ts
+++ section-07/src/app.ts
@@ -5,7 +5,7 @@
 //   setTimeout(() => resolve("This is done"), 2000);
 // });
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return { ...objA, ...objB };
 }
 
