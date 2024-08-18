ec088e9dc9976b1a73a475d0992015ca3104ef07
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-18 21:13:40 +0530

    chore: Creating a generic function

--- section-07/src/app.ts
+++ section-07/src/app.ts
@@ -1,6 +1,14 @@
 // Generics - Its a way to create reusable components that can work with any data type and not just one data type.
-const names: Array<string> = []; // string[]
+// const names: Array<string> = []; // string[]
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("This is done"), 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("This is done"), 2000);
+// });
+
+function merge<T, U>(objA: T, objB: U): T & U {
+  return { ...objA, ...objB };
+}
+
+const mergeObj = merge({ name: 'Aman', hobbies: ['Sports'] }, { age: 34 });
+// mergeObj.name; // This will not work because merge function is returning object type and not the object with name property.
+console.log(mergeObj);
