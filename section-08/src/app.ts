2439e69c7fc7035dc80b9e3a07bcfdc3ff4b5e67
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-19 15:45:44 +0530

    chore: Diving into property decorators

--- section-08/src/app.ts
+++ section-08/src/app.ts
@@ -36,3 +36,31 @@ class Person {
 
 const person = new Person();
 console.log(person);
+// ----------------------------
+function Log(target: any, propertyName: string | symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set setPrice(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getProductPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
