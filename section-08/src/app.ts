94cf128f993369954a6a15d4deda0b3126bff903
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-19 17:13:17 +0530

    chore: Creating an Autobind decorator

--- section-08/src/app.ts
+++ section-08/src/app.ts
@@ -96,3 +96,31 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+// ----------------------------
+// Autobind Decorator
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return originalMethod.bind(this);
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
+// button.addEventListener("click", new Printer().showMessage.bind(p));
