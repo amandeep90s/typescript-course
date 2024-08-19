9819ae9e6607e003ba8fced64698e234f612d5ec
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-19 19:51:33 +0530

    chore: Dom element selection & oop rendering

--- section-09/src/app.ts
+++ section-09/src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input",
+    ) as HTMLTemplateElement; // type casting
+    this.hostElement = document.getElementById("app") as HTMLDivElement; // type casting
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement; // type casting
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
