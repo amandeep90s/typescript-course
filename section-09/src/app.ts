096b660550e5a962468841f2aaadf0055a1eb538
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-02 12:46:03 +0530

    feat: Creating a re-usable validation functionality

--- section-09/src/app.ts
+++ section-09/src/app.ts
@@ -1,8 +1,39 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+  const { value, required, maxLength, minLength, max, min } = validatableInput;
+  if (required) {
+    isValid = isValid && value.toString().trim().length !== 0;
+  }
+  if (minLength != null && typeof value === 'string') {
+    isValid = isValid && value.length >= minLength;
+  }
+  if (maxLength != null && typeof value === 'string') {
+    isValid = isValid && value.length <= maxLength;
+  }
+  if (min != null && typeof value === 'number') {
+    isValid = isValid && value >= min;
+  }
+  if (max != null && typeof value === 'number') {
+    isValid = isValid && value <= max;
+  }
+  return isValid;
+}
+
 // Autobind Decorator
 function Autobind(
   _target: any,
   _methodName: string,
-  descriptor: PropertyDescriptor,
+  descriptor: PropertyDescriptor
 ) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
@@ -25,24 +56,24 @@ class ProjectInput {
 
   constructor() {
     this.templateElement = document.getElementById(
-      "project-input",
+      'project-input'
     ) as HTMLTemplateElement; // type casting
-    this.hostElement = document.getElementById("app") as HTMLDivElement; // type casting
+    this.hostElement = document.getElementById('app') as HTMLDivElement; // type casting
     const importedNode = document.importNode(
       this.templateElement.content,
-      true,
+      true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement; // type casting
-    this.element.id = "user-input";
+    this.element.id = 'user-input';
 
     this.titleInputElement = this.element.querySelector(
-      "#title",
+      '#title'
     ) as HTMLInputElement; // type casting
     this.descriptionInputElement = this.element.querySelector(
-      "#description",
+      '#description'
     ) as HTMLInputElement;
     this.peopleInputElement = this.element.querySelector(
-      "#people",
+      '#people'
     ) as HTMLInputElement;
     this.configure();
     this.attach();
@@ -53,21 +84,37 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
-      alert("Invalid input, please try again!");
+      alert('Invalid input, please try again!');
       return;
     }
     return [enteredTitle, enteredDescription, +enteredPeople];
   }
 
   private clearInputs() {
-    this.titleInputElement.value = "";
-    this.descriptionInputElement.value = "";
-    this.peopleInputElement.value = "";
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
   }
 
   @Autobind
@@ -82,11 +129,11 @@ class ProjectInput {
   }
 
   private configure() {
-    this.element.addEventListener("submit", this.submitHandler);
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
 
